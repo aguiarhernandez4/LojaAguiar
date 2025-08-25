5dd6a0679df0ec4edb506fe064f4c8e67d2bcff4
Hernandez <aguiarhernandez4@gmail.com>
2025-08-25 14:51:10 -0300

    atualizando entity produtos

--- src/main/java/com/lojaaguiar/projeto/entity/Produtos.java
+++ src/main/java/com/lojaaguiar/projeto/entity/Produtos.java
@@ -2,19 +2,41 @@ package com.lojaaguiar.projeto.entity;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 
 @Entity
 public class Produtos {
     @Id
-    @GeneratedValue
-    private Long id;
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
     private String name;
     private int price;
     private String descricao;
 
-    public int getId(){
-        return id;
+    public int getId(){return id;}
+    public void setId(int id) {this.id = id;}
+
+    public String getName() {return name;}
+    public void setName(String name){this.name = name;}
+
+    public int getPrice(){return price;}
+    public void setPrice(int price) {this.price = price;}
+
+    public String getDescricao() {return descricao;}
+    public void setDescricao(String descricao) {this.descricao = descricao;}
+
+    public Produtos(int id, String name, int price, String descricao) {
+        super();
+        this.id = id;
+        this.name = name;
+        this.price = price;
+        this.descricao = descricao;
     }
+
+    public Produtos(){
+        super();
+    }
+
 
 }
