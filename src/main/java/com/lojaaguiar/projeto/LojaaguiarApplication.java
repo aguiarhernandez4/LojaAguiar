7140366b114066bc3842f8ebb0487993b31e798b
Hernandez <aguiarhernandez4@gmail.com>
2025-08-25 11:51:13 -0300

    first commit

---
+++ src/main/java/com/lojaaguiar/projeto/LojaaguiarApplication.java
@@ -0,0 +1,13 @@
+package com.lojaaguiar.projeto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LojaaguiarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LojaaguiarApplication.class, args);
+	}
+
+}
