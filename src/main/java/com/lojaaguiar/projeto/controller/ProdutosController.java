b58c1c261a401963a129b359c0811928793e260e
Hernandez <aguiarhernandez4@gmail.com>
2025-08-27 20:49:58 -0300

    atualizando com sql

--- src/main/java/com/lojaaguiar/projeto/controller/ProdutosController.java
+++ src/main/java/com/lojaaguiar/projeto/controller/ProdutosController.java
@@ -3,7 +3,13 @@ package com.lojaaguiar.projeto.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.servlet.ModelAndView;
+import java.util.List;
 import org.springframework.web.bind.annotation.GetMapping;
+
+
+
 
 import com.lojaaguiar.projeto.service.ProdutosService;
 import com.lojaaguiar.projeto.entity.Produtos;
@@ -12,10 +18,19 @@ public class ProdutosController {
     @Autowired
     ProdutosService produtoService;
 
-    @GetMapping("/save")
+    @PostMapping("/save")
     public String addProdutos(@ModelAttribute Produtos p) {
         produtoService.save(p);
         return "redirect:produtos";
+    }
+
+    @GetMapping("produtos-list")
+    public ModelAndView availableProdutos() {
+        List<Produtos> listOfProdutos = produtoService.getAllProdutos();
+        ModelAndView mav = new ModelAndView();
+        mav.setViewName("Produtos");
+        mav.addObject("Produto", listOfProdutos);
+        return mav;
     }
 
 }
