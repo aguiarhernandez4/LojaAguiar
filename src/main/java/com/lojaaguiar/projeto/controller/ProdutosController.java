9961376e9fcd1b19f8bc0e38a05c79b4fe34d9c6
Hernandez <aguiarhernandez4@gmail.com>
2025-08-31 12:04:05 -0300

    removendo myrepository and my service

--- src/main/java/com/lojaaguiar/projeto/controller/ProdutosController.java
+++ src/main/java/com/lojaaguiar/projeto/controller/ProdutosController.java
@@ -3,18 +3,13 @@ package com.lojaaguiar.projeto.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import java.util.List;
-import org.springframework.ui.Model;
 
-import com.lojaaguiar.projeto.service.MyProdutosService;
+// import com.lojaaguiar.projeto.service.MyProdutosService;
 import com.lojaaguiar.projeto.service.ProdutosService;
 import com.lojaaguiar.projeto.entity.Produtos;
-import com.lojaaguiar.projeto.repository.MyProdutosRepository;
+// import com.lojaaguiar.projeto.repository.MyProdutosRepository;
 import com.lojaaguiar.projeto.repository.ProdutosRepository;
 
 @Controller
@@ -23,23 +18,11 @@ public class ProdutosController {
     ProdutosService produtoService;
 
     @Autowired
-    MyProdutosService MyProdutosListService;
-
-    @Autowired
-    MyProdutosRepository myProdutosRepo;
-
-    @Autowired
     ProdutosRepository Produtos;
 
     @PostMapping("/save")
     public String addProdutos(@ModelAttribute Produtos p) {
         produtoService.save(p);
         return "redirect:produtos";
-    }
-
-    @RequestMapping("/deleteprodutos/{id}")    
-    public String getMyList(@PathVariable("id") int id) {
-        produtoService.deleteProdutosById(id);
-        return "redirect:/produtos";
     }
 }
