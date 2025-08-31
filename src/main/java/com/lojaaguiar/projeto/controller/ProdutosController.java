ff3d07a3aac01ef79299c37c928c5ec7a9163720
Hernandez <aguiarhernandez4@gmail.com>
2025-08-31 17:09:10 -0300

    buscando esse maldito erro

--- src/main/java/com/lojaaguiar/projeto/controller/ProdutosController.java
+++ src/main/java/com/lojaaguiar/projeto/controller/ProdutosController.java
@@ -1,28 +1,29 @@
 package com.lojaaguiar.projeto.controller;
 
-
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
-
+import org.springframework.ui.Model;
 // import com.lojaaguiar.projeto.service.MyProdutosService;
 import com.lojaaguiar.projeto.service.ProdutosService;
 import com.lojaaguiar.projeto.entity.Produtos;
 // import com.lojaaguiar.projeto.repository.MyProdutosRepository;
-import com.lojaaguiar.projeto.repository.ProdutosRepository;
-
 @Controller
 public class ProdutosController {
     @Autowired
     ProdutosService produtoService;
 
-    @Autowired
-    ProdutosRepository Produtos;
-
     @PostMapping("/save")
     public String addProdutos(@ModelAttribute Produtos p) {
         produtoService.save(p);
         return "redirect:produtos";
+    }
+
+    @GetMapping("/mostrarprodutos")
+    public String listarProdutos(Model model) {
+        model.addAttribute("mostrarprodutos", produtoService.getAllProdutos());
+        return "produtos";
     }
 }
--- src/main/java/com/lojaaguiar/projeto/entity/MyListProdutos.java
+++ src/main/java/com/lojaaguiar/projeto/entity/MyListProdutos.java
@@ -7,7 +7,7 @@ import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 
 @Entity
-@Table(name = "produtos")
+@Table(name = "my_list_produtos")
 public class MyListProdutos {
     @Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
