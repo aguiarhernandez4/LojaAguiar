51f4ee74b86b68775f4ca945a52fc7f31429f87d
JOoão Pedro <aguiarhernandez4@gmail.com>
2025-10-16 16:32:31 -0300

    atualizando a entidade usuario com spring e lombok

--- src/main/java/com/lojaaguiar/projeto/entity/Usuario.java
+++ src/main/java/com/lojaaguiar/projeto/entity/Usuario.java
@@ -10,9 +10,15 @@ import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.NoArgsConstructor;
 
 
 @Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 @Table(name = "usuarios")
 public class Usuario {
     @Id
@@ -26,33 +32,4 @@ public class Usuario {
     private String email;
     @Column(name = "password", length = 15, nullable = false, unique = false)
     private String password;
-
-    public int getId() { return id; }
-    public void setId(int id) { this.id = id; }
-
-    public String getNome() { return nome; }
-    public void setNome(String nome) { this.nome = nome; }
-
-    public String getCpf() { return cpf; }
-    public void setCpf(String cpf) { this.cpf = cpf; }
-
-    public String getEmail() { return email; }
-    public void setEmail(String email) { this.email = email; }
-
-    public String getPassword() { return password; }
-    public void setPassword(String password) { this.password = password; }
-
-    public Usuario(int id, String nome, String cpf, String email, String password) { 
-        super();
-
-        this.id = id;
-        this.nome = nome;
-        this.cpf = cpf;
-        this.email = email;
-        this.password = password;
-    }
-
-    public Usuario() {
-        super();
-    }
 }
