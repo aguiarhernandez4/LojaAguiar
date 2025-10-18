1ae13405d4f38aa3a1d8ff51d05e480baed16b3d
JOoão Pedro <aguiarhernandez4@gmail.com>
2025-10-18 20:04:24 -0300

    adicionando uma espécie de filtragem no repositório

--- src/main/java/com/lojaaguiar/projeto/repository/UsuariosRepository.java
+++ src/main/java/com/lojaaguiar/projeto/repository/UsuariosRepository.java
@@ -5,7 +5,16 @@ import org.springframework.stereotype.Repository;
 
 import com.lojaaguiar.projeto.entity.Usuario;
 
+import java.util.Optional;
+
 @Repository
 public interface UsuariosRepository extends JpaRepository<Usuario, Integer>{
+    <Usuario>
+
+    Optional<Usuario> findByEmail(String email);
+
+    boolean existsByEmail(String email);
+
+    boolean existsByCpf(String cpf);
 
 }
