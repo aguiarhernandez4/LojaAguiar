afaa8eb7206c9eea6ea2892102d3a9905d5cfcca
JOoão Pedro <aguiarhernandez4@gmail.com>
2025-10-18 21:55:19 -0300

    apenas pequenos ajustes

--- src/main/java/com/lojaaguiar/projeto/repository/UsuariosRepository.java
+++ src/main/java/com/lojaaguiar/projeto/repository/UsuariosRepository.java
@@ -9,7 +9,6 @@ import java.util.Optional;
 
 @Repository
 public interface UsuariosRepository extends JpaRepository<Usuario, Integer>{
-    <Usuario>
 
     Optional<Usuario> findByEmail(String email);
 
