f59e8be78bbc7412a972d379cfde67717c6094a6
JOoão Pedro <aguiarhernandez4@gmail.com>
2025-10-20 18:48:24 -0300

    pequenas mudanças

--- src/main/java/com/lojaaguiar/projeto/controller/ProdutosController.java
+++ src/main/java/com/lojaaguiar/projeto/controller/ProdutosController.java
@@ -1,26 +1,26 @@
 package com.lojaaguiar.projeto.controller;
 
+import com.lojaaguiar.projeto.repository.ProdutosRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.http.MediaType;
 
 import com.lojaaguiar.projeto.service.ProdutosService;
 import com.lojaaguiar.projeto.entity.Produtos;
 
+
 @Controller
 public class ProdutosController {
     @Autowired
-    ProdutosService produtoService;
+    private ProdutosService produtoService;
 
+    @Autowired
+    private ProdutosRepository produtoRepo;
 
     @PostMapping("/save")
     public String addProdutos(@ModelAttribute Produtos p) {
@@ -48,4 +48,16 @@ public class ProdutosController {
         return "editproduto";
     }
 
+    @GetMapping("/{id}")
+    public ResponseEntity<byte[]> getImagem(@PathVariable int id){
+        Produtos produto = produtoRepo.findById(id).orElse(null);
+
+        if (produto == null || produto.getImagem() == null) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
+        }
+
+        HttpHeaders headers = new HttpHeaders();
+        headers.add("Content-Type", "image/jpeg");
+        return new ResponseEntity<>(produto.getImagem(), headers, HttpStatus.OK);
+    }
 }
